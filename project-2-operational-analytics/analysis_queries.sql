a1b61a78e60c07d84094de3eab619c00c1bc31c7
RudAgn13 <roodagnihotri@gmail.com>
2026-06-02 03:39:32 +0530

    Update analysis_queries.sql

--- project-2-operational-analytics/analysis_queries.sql
+++ project-2-operational-analytics/analysis_queries.sql
@@ -11,3 +11,24 @@ on o.order_id = oi.order_id
 where o.status='delivered'
 group by date_trunc('month', o.order_date)
 order by "month" asc;
+
+-- METRIC 2: CATEGORY PERFORMANCE OVER TIME
+with x as
+(
+  select
+    oi.product_id,
+    date_trunc('month', o.order_date) as sales_month,
+    sum(oi.quantity * oi.unit_price) as revenue
+  from orders o
+  join order_items oi on o.order_id = oi.order_id
+  where o.status = 'delivered'
+  group by oi.product_id, date_trunc('month', o.order_date)
+)
+select
+  p.category_l1,
+  x.sales_month as "month",
+  coalesce(sum(x.revenue), 0) as categoryrevenue_by_month
+from products p
+left join x on p.product_id = x.product_id
+group by p.category_l1, x.sales_month
+order by p.category_l1, "month" asc;
