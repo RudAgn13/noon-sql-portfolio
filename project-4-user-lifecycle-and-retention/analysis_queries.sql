0907748753cfea7911a859206b1791c100e843ac
RudAgn13 <roodagnihotri@gmail.com>
2026-06-02 16:11:32 +0530

    Create analysis_queries.sql

---
+++ project-4-user-lifecycle-and-retention/analysis_queries.sql
@@ -0,0 +1,35 @@
+-- TARGET DATABASE: PostgreSQL v14
+-- Project 4: User Cohort Lifecycle & Retention Analytics
+
+-- METRIC 1: USER RETENTION COHORT MATRIX
+with fpd as
+(
+  select
+  	  c.customer_id,
+      c.full_name,
+      date_trunc('month',min(o.order_date)) first_delivered_order_month
+  from customers c
+  join orders o
+  on c.customer_id = o.customer_id
+  where o.status = 'delivered'
+  group by c.customer_id
+  order by first_delivered_order_month
+)
+,cte as
+(
+  select
+      fpd.customer_id,
+      fpd.full_name,
+      fpd.first_delivered_order_month,
+      date_trunc('month',o.order_date) order_month
+  from orders o
+  join fpd
+  on o.customer_id = fpd.customer_id
+)
+select
+    first_delivered_order_month,
+    (((extract(year from order_month) - extract(year from first_delivered_order_month))*12 + extract(month from order_month) - extract(month from first_delivered_order_month))) month_gap,
+    count(distinct customer_id) cohort_size
+from cte
+group by first_delivered_order_month, month_gap
+--IMPORTANT: where month_gap=0, cohort_size tells total active users in that month. repeat customers are shown in rows where month gap>0
