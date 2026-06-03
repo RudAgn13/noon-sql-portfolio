523412a99a515ea72ef6b863b553f6a9a463ac2b
RudAgn13 <roodagnihotri@gmail.com>
2026-06-03 16:35:31 +0530

    Update analysis_queries.sql

--- project-8-marketing-campaigns/analysis_queries.sql
+++ project-8-marketing-campaigns/analysis_queries.sql
@@ -24,3 +24,44 @@ order by
     when 'campaign' then 2
     else 3
   end;
+
+-- METRIC 2: CAMPAIGN UPLIFT BY CATEGORY
+with non_campaign_data as
+(
+select
+  	p.category_l1,
+	date_trunc('month', o.order_date) sales_month,
+    sum(oi.quantity*oi.unit_price) revenue
+from orders o
+join order_items oi on o.order_id = oi.order_id
+join products p on oi.product_id = p.product_id
+where o.order_date < '2024-11-01' or o.order_date > '2024-11-30'
+group by p.category_l1, date_trunc('month', o.order_date)
+order by p.category_l1, sales_month
+) 
+, campaign_data as
+(
+select
+  	p.category_l1,
+	date_trunc('month', o.order_date) sales_month,
+    sum(oi.quantity*oi.unit_price) revenue
+from orders o
+join order_items oi on o.order_id = oi.order_id
+join products p on oi.product_id = p.product_id
+where date_trunc('month', o.order_date) = '2024-11-01'
+group by p.category_l1, date_trunc('month', o.order_date)
+order by sales_month
+)
+, non_campaign_average as
+(
+select
+	category_l1,
+  	avg(revenue) avg_revenue
+from non_campaign_data
+group by category_l1
+)
+select
+	cd.category_l1,
+	round(100.0*(cd.revenue-nca.avg_revenue)/nca.avg_revenue,2) uplift_pctg
+from non_campaign_average nca
+join campaign_data cd on nca.category_l1 = cd.category_l1;
