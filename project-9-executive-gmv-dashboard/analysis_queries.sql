161fa3e4d1b6aa6e9bce5a2cb79c972f86b8de7e
RudAgn13 <roodagnihotri@gmail.com>
2026-06-04 16:38:00 +0530

    Update analysis_queries.sql

--- project-9-executive-gmv-dashboard/analysis_queries.sql
+++ project-9-executive-gmv-dashboard/analysis_queries.sql
@@ -1,1 +1,15 @@
 
+
+
+-- METRIC 1: OVERALL BUSINESS HEALTH SUMMARY
+select
+    sum(oi.quantity * oi.unit_price) total_gmv,
+    sum(case when o.status = 'delivered' then oi.quantity*oi.unit_price else 0 end) delivered_revenue,
+    round(100.0*count(distinct case when o.status = 'cancelled' then o.order_id end)/count(distinct o.order_id), 2) cancellation_rate,
+    round(100.0*count(distinct case when o.status = 'returned' then o.order_id end)/count(distinct o.order_id), 2) return_rate,
+    --not using returns table here because that is a product-level return history while order-level return history lies in orders.status='returned'
+    count(distinct o.customer_id) active_customers,
+    count(distinct p.seller_id) active_sellers
+from orders o
+join order_items oi on o.order_id = oi.order_id
+join products p on oi.product_id = p.product_id;
